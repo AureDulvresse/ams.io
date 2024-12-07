e3d61025fe09e3a90326751f352ade0df812a008
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-07 22:32:37 +0100

    Prod Test Project

--- app/hr/staff/[uuid]/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StaffUpdate = () => {
-  return <div>StaffUpdate</div>;
-};
-
-export default StaffUpdate;
--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,101 @@
-"use client";
-import React, { useState } from "react";
-import { GraduationCapIcon, Users2, WalletIcon } from "lucide-react";
-import ActivitySummary from "@/components/common/ActivitySummery";
-import FiliereChart from "@/components/common/FiliereChart";
-import FinancialChart from "@/components/common/FinancialChart";
-import ScheduleCalendar from "@/components/common/ScheduleCalendar";
-import StatCard from "@/components/common/StatCard";
-import Sidebar from "@/components/partials/Sidebar";
-import Topbar from "@/components/partials/Topbar";
-import useFetchData from "@/hooks/useFetchData";
+import Image from "next/image";
 
-const Home = async () => {
-  const {
-    data: events,
-    isLoading: isLoadingEvent,
-    error,
-  } = useFetchData<Event[]>("/api/events");
-
+export default function Home() {
   return (
-    <div className="flex items-start min-h-screen w-full bg-slate-50 dark:bg-gray-800">
-      <Sidebar />
-      <main
-        className={`transition-all duration-300 ml-64 py-1.5 w-full`}
-      >
-        <Topbar />
-        <div className="p-2 mt-[70px] w-full grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {/* Statistiques en haut */}
-          <div className="lg:col-span-3 mb-4">
-            <h2 className="text-2xl font-bold font-fredoka text-indigo-500 mb-6">
-              Overview
-            </h2>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-6">
-              <StatCard
-                libelle={"Nombre total étudiant"}
-                data={1200}
-                icon={GraduationCapIcon}
-              />
-              <StatCard libelle={"Nombre personnel"} data={160} icon={Users2} />
-              <StatCard
-                libelle={"Revenu total"}
-                data={230600}
-                icon={WalletIcon}
-                unity="XAF"
-              />
-              <StatCard
-                libelle={"Dépense annuelle"}
-                data={290600}
-                icon={WalletIcon}
-                unity="XAF"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="p-2 w-full grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {/* Graphiques en bas */}
-          <div className="lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
-            <FinancialChart />
-            <FiliereChart />
-          </div>
-        </div>
-        <div className="p-2 w-full grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <div className="lg:col-span-2">
-            <ScheduleCalendar
-              className={"h-[450px]"}
-              data={events || []}
-              isLoading={isLoadingEvent}
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
             />
-          </div>
-          <div className="lg:col-span-1">
-            <ActivitySummary />
-          </div>
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
       </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
