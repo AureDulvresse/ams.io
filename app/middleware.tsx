b780b42cf050a57cf6261248dbf0c9d7d4f1d831
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-20 16:23:50 +0100

    Update middleware.tsx

--- app/middleware.tsx
+++ app/middleware.tsx
@@ -1,16 +1,17 @@
 import { auth as middleware } from "@/src/lib/auth"
 
 export default middleware((req) => {
-   
+
    if (!req.auth && req.nextUrl.pathname !== "/login") {
       const newUrl = new URL("/login", req.nextUrl.origin)
       return Response.redirect(newUrl)
    }
+   
 })
 
 export const config = {
    matcher: [
-      "(dashboard)/:path*",
+      "dashboard/:path*",
       "/((?!api|_next/static|_next/image|favicon.ico).*)"
    ],
 }
