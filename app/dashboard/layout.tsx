e3e9feb41fb004a617bdbcb930d6e29582458ca9
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-21 16:53:09 +0100

    middleware updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,18 @@
 import { SidebarInset, SidebarProvider } from "@/src/components/ui/sidebar";
 import { AppSidebar } from "@/src/components/layouts/sidebar/app-sidebar";
 import TopBar from "@/src/components/layouts/topbar/top-bar";
+import { auth } from "@/src/lib/auth";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
    children,
 }: {
    children: React.ReactNode
 }) {
-   return <section>
+
+   const session = await auth();
+   const user = session?.user;
+
+   return <>
       <SidebarProvider>
          <AppSidebar />
          <SidebarInset>
@@ -15,5 +20,5 @@ export default function DashboardLayout({
             {children}
          </SidebarInset>
       </SidebarProvider>
-   </section>
+   </>
 }
---
+++ app/middleware.ts
@@ -0,0 +1,37 @@
+import { auth } from "@/src/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login", "/"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const session = await auth();
+
+  if (!session?.user && path !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (isProtectedRoute && !session?.user) {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (
+    isPublicRoute &&
+    session?.user &&
+    !req.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
