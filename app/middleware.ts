f9e78cd83fccc1999b5ad04a5369219997bc651c
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-23 12:06:51 +0100

    Update middleware.ts

--- app/middleware.ts
+++ app/middleware.ts
@@ -31,7 +31,4 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+ 
