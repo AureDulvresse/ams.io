791c648a8046f8885777d66ab860087f9d543e14
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-24 12:54:18 +0100

    project mise à jour

--- app/login/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginLayout({
-   children,
-}: {
-   children: React.ReactNode
-}) {
-   return <section className="fixed top-0 left-0 z-50 w-screen">{children}</section>
-}
