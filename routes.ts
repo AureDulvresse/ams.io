23a6229ad2c839da56c242565c292ea555c933e8
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-12-27 08:33:38 +0100

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -1,6 +1,6 @@
 // Configuration des routes
 export const publicRoutes = ["/"];
-export const authRoutes = ["/login"];
+export const authRoutes = ["/auth/login"];
 export const privateRoutes = ["/dashboard"];
 export const apiAuthPrefix = "/api/auth";
 export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
