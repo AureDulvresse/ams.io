6721581b4ec7e88f1291bd42127b9332903abfed
Aure <147180124+AureDulvresse@users.noreply.github.com>
2025-01-03 10:26:26 +0100

    Update director-dashboard.tsx

--- app/(protected)/dashboard/_components/director-dashboard.tsx
+++ app/(protected)/dashboard/_components/director-dashboard.tsx
@@ -43,7 +43,7 @@ const DirectorDashboard = () => {
             />
          </div>
 
-         <div className="grid grid-cols-2 gap-2">
+         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <IncomeChart className="col-span-1" />
             <ExpenseChart className="col-span-1" />
          </div>
